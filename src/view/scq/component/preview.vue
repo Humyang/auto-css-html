8f3f272576756337cb1847f5c249ba15bc20069a
杨雄贵 <378029633@qq.com>
2019-10-28 18:31:04 +0800

    1

--- src/view/scq/component/preview.vue
+++ src/view/scq/component/preview.vue
@@ -1,65 +1,64 @@
 <template>
-    <div 
-    @click.stop="actionClick" 
-    class="preview flex" 
+  <div
+    @click.stop="actionClick"
+    class="preview flex"
     :class="{'row':dataset.direction=='row',
     'column':dataset.direction=='column',
     'selected':onSelected,
     [dataset.className]:true
-    }" 
-    :style="{width:100+'%'}" >
-        {{dataset.direction}}
-        <template v-if="dataset.subset!=undefined" >
-            <preview 
-                v-for="item,index in dataset.subset" 
-                :dataset="item" 
-                :current_length="clength" 
-                :key="index"
-                :tagType="item.tagType"
-                :direction="item.direction"
-                v-on:current="getCurrent"
-                :currentSelect="currentSelect"
-            >
-            </preview>
-        </template>
-        
-    </div>
+    }"
+    :style="{width:100+'%',height:'90%'}"
+  >
+    {{dataset.direction}}
+    <template v-if="dataset.subset!=undefined">
+      <preview
+        v-for="item,index in dataset.subset"
+        :dataset="item"
+        :current_length="clength"
+        :key="index"
+        :tagType="item.tagType"
+        :direction="item.direction"
+        v-on:current="getCurrent"
+        :currentSelect="currentSelect"
+      ></preview>
+    </template>
+  </div>
 </template>
 <script>
 // import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: "preview",
-  props:['dataset','current_length','direction','currentSelect','tagType'],
-  data(){
-      return {
-          clength:1
-      }
+  name: 'preview',
+  props: ['dataset', 'current_length', 'direction', 'currentSelect', 'tagType'],
+  data() {
+    return {
+      clength: 1
+    }
   },
-  methods:{
-        actionClick(){
-            // console.log(this.dataset.id)
-            this.$emit('current',{id:this.dataset.id,tagType:this.tagType})
-        },
-        getCurrent(event){
-            this.$emit('current',event)
-        }
+  methods: {
+    actionClick() {
+      // console.log(this.dataset.id)
+      this.$emit('current', { id: this.dataset.id, tagType: this.tagType })
+    },
+    getCurrent(event) {
+      this.$emit('current', event)
+    }
   },
-  computed:{
-      onSelected(){
-          console.log(this.currentSelect)
-          let result = this.currentSelect.find((item)=>{
-              return item == this.dataset.id
-          })
-          return result!=undefined?true:false
-      }
+  computed: {
+    onSelected() {
+      console.log(this.currentSelect)
+      let result = this.currentSelect.find(item => {
+        return item == this.dataset.id
+      })
+      return result != undefined ? true : false
+    }
   },
-  updated:function(){
+  updated: function() {
     //   计算当前自己元素数量
     // console.log(this.dataset.subset.length)
     this.clength = this.dataset.subset && this.dataset.subset.length
 
     // console.log(this.current_length)
   }
-};
+}
 </script>
--- src/view/scq/index.vue
+++ src/view/scq/index.vue
@@ -1,140 +1,145 @@
 
 <template>
   <div class="scq-view">
-      <div class="current flex row " style="width:100%;">
-        <div>按下多选：{{isMulitle}}</div> 
-        <div> 当前选中：{{current_id}}</div>
+    <div class="current flex row" style="width:100%;">
+      <div>按下多选：{{isMulitle}}</div>
+      <div>当前选中：{{current_id}}</div>
+      <div>
+        <el-select placeholder="请选择活动区域" v-model="tagType" @change="currentTagChange">
+          <el-option v-for="(item,index) in tageTypeArray" :key="index" :label="item" :value="item"></el-option>
+        </el-select>
+      </div>
+      <div class="flex row">
+        containt name
+        <el-input v-model="containerName" placeholder="请输入内容"></el-input>
+      </div>
+    </div>
+    <div class="flex row content">
+      <div style="height:200px;width: 100%;">
+        <preview
+          v-on:current="getCurrent"
+          :dataset="dataset[0]"
+          class="grow paremt_preview"
+          :currentSelect="current_id"
+          :tagType="dataset[0].tagType"
+        ></preview>
+      </div>
+      <div class="insersub-view flex column" style="margin-left:20px;">
         <div>
-          <el-select  placeholder="请选择活动区域" v-model="tagType" @change="currentTagChange">
-            <el-option v-for="(item,index) in tageTypeArray" :key="index" :label="item" :value="item"></el-option>
+          <el-select placeholder="设置添加位置" v-model="appendPosition">
+            <el-option label="直接子级直接添加" value="subChildAppend"></el-option>
+            <el-option label="直接子级插入" value="subChildInsert"></el-option>
           </el-select>
         </div>
         <div class="flex row">
-          containt name <el-input v-model="containerName" placeholder="请输入内容"></el-input>
+          <div class="row-list">
+            <div class="subitem flex vcenter" @click="rowAdd(0)">ROW</div>
+            <div
+              class="subitem flex vcenter"
+              v-for="(item,index) in rowList"
+              @click="rowAdd(item)"
+            >{{item}}</div>
+          </div>
+          <div class="column-list">
+            <div class="subitem flex vcenter" @click="columnAdd(0)">COLUMN</div>
+            <div
+              class="subitem flex vcenter"
+              v-for="item in columnList"
+              @click="columnAdd(item)"
+            >{{item}}</div>
+          </div>
         </div>
       </div>
-      <div class="flex row content">
-        <preview v-on:current="getCurrent" 
-        :dataset="dataset[0]" 
-        class="grow paremt_preview" 
-        :currentSelect="current_id"
-        :tagType="dataset[0].tagType"
-        ></preview>
-        <div class="insersub-view flex column" style="margin-left:20px;">
-            <div>
-              <el-select  placeholder="设置添加位置" v-model="appendPosition" >
-                <el-option  label="直接子级直接添加" value="subChildAppend"></el-option>
-                <el-option  label="直接子级插入" value="subChildInsert"></el-option>
-              </el-select>
-            </div>
-            <div class="flex row">
-              <div class="row-list">
-                  <div class="subitem flex vcenter" @click="rowAdd(0)">ROW</div>
-                  <div class="subitem flex vcenter"  v-for="(item,index) in rowList" @click="rowAdd(item)">
-                      {{item}}
-                  </div>
-              </div>
-              <div class="column-list">
-                      <div class="subitem flex vcenter" @click="columnAdd(0)">COLUMN</div>
-                  <div class="subitem flex vcenter" v-for="item in columnList" @click="columnAdd(item)">
-                      {{item}}
-                  </div>
-              </div>
-            </div>
-        </div>
-      </div>
-      
-      <textarea name="" id="" cols="30" rows="10" class="html-box" v-model="htmlcode">
-            
-      </textarea>
-      <div class="css-box">
-          <code class="css-code" v-html="csscode">
-          </code>
-      </div>
+    </div>
+
+    <textarea name id cols="30" rows="10" class="html-box" v-model="htmlcode"></textarea>
+    <div class="css-box">
+      <code class="css-code" v-html="csscode"></code>
+    </div>
   </div>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld";
-import preview from "./component/preview";
-import uid2 from "uid2";
-import classname from "classname";
+import preview from './component/preview'
+import uid2 from 'uid2'
+import classname from 'classname'
 export default {
-  name: "index",
+  name: 'index',
   data() {
     return {
-      current_id: ["1"],
-      appendPosition: "subChildAppend",
+      current_id: ['1'],
+      appendPosition: 'subChildAppend',
       dataset: [
         {
-          id: "1",
-          value: "1",
-          tagType: "div",
-          direction: "row",
-          className: "",
+          id: '1',
+          value: '1',
+          tagType: 'div',
+          direction: 'row',
+          className: '',
           subset: []
         }
       ],
-      containerName: "container",
-      addDirection: "row",
+      containerName: 'container',
+      addDirection: 'row',
       addNumber: 1,
-      tagType: "div",
+      tagType: 'div',
       rowList: [1, 2, 3, 4, 5],
       columnList: [1, 2, 3, 4, 5],
-      tageTypeArray: ["div", "view", "image", "text","span"],
+      tageTypeArray: ['div', 'view', 'image', 'text', 'span'],
       isMulitle: false
-    };
+    }
   },
   methods: {
     currentTagChange(event) {
-      console.log(event);
-      this.getNodeByIdChaneType(this.dataset, this.current_id, event);
+      console.log(event)
+      this.getNodeByIdChaneType(this.dataset, this.current_id, event)
     },
     getNodeByIdChaneType(subset, id, tagType) {
       let item = subset.filter(item => {
         // return item.id == id
         let findresult = id.find(subitem => {
-          return subitem == item.id;
-        });
-        console.log(!!findresult);
-        return !!findresult;
-      });
+          return subitem == item.id
+        })
+        console.log(!!findresult)
+        return !!findresult
+      })
       if (item.length > 0) {
         item.forEach(subitem => {
-          subitem.tagType = tagType;
-        });
-        this.setDatasetClassName(this.dataset, 1);
+          subitem.tagType = tagType
+        })
+        this.setDatasetClassName(this.dataset, 1)
       } else {
         subset.map(item => {
-          this.getNodeByIdChaneType(item.subset, id, tagType);
-        });
+          this.getNodeByIdChaneType(item.subset, id, tagType)
+        })
       }
     },
     getCurrent(event) {
-      console.log(event);
-      this.tagType = event.tagType;
+      console.log(event)
+      this.tagType = event.tagType
       if (this.isMulitle) {
         let finditem = this.current_id.find(item => {
-          return item == event.id;
-        });
+          return item == event.id
+        })
 
         // console.log(finditem);
         if (finditem === undefined) {
-          this.current_id.push(event.id);
+          this.current_id.push(event.id)
         } else {
           // console.log(finditem)
           this.current_id.splice(
             this.current_id.findIndex(item => {
-              return item === finditem;
+              return item === finditem
             }),
             1
-          );
+          )
         }
 
-        let newarray = JSON.parse(JSON.stringify(this.current_id));
-        this.current_id = newarray;
+        let newarray = JSON.parse(JSON.stringify(this.current_id))
+        this.current_id = newarray
       } else {
-        this.current_id = JSON.parse(JSON.stringify([event.id]));
+        this.current_id = JSON.parse(JSON.stringify([event.id]))
       }
     },
     //   遍历树结构，找到指定值
@@ -142,192 +147,185 @@ export default {
       let item = subset.filter(item => {
         // return item.id == id
         let findresult = id.find(subitem => {
-          return subitem == item.id;
-        });
-        console.log(!!findresult);
-        return !!findresult;
-      });
+          return subitem == item.id
+        })
+        console.log(!!findresult)
+        return !!findresult
+      })
       if (item.length > 0) {
-        this.addElement(item);
+        this.addElement(item)
       } else {
         subset.map(item => {
-          this.getNodeById(item.subset, id);
-        });
+          this.getNodeById(item.subset, id)
+        })
       }
     },
     columnAdd(number) {
-      this.addDirection = "column";
-      this.addNumber = number;
-      this.getNodeById(this.dataset, this.current_id);
+      this.addDirection = 'column'
+      this.addNumber = number
+      this.getNodeById(this.dataset, this.current_id)
     },
     rowAdd(number) {
-      this.addNumber = number;
-      this.addDirection = "row";
+      this.addNumber = number
+      this.addDirection = 'row'
       //   console.log( this.current_id)
-      this.getNodeById(this.dataset, this.current_id);
+      this.getNodeById(this.dataset, this.current_id)
       //   this.dataset.subset.push({
       //               id:"2.3",
       //               value:"2.3"
       //           })
     },
-    resetUid(dataset){
+    resetUid(dataset) {
       console.log(dataset)
-      if(dataset.length>0){
-        
-        dataset.forEach((item)=>{
+      if (dataset.length > 0) {
+        dataset.forEach(item => {
           console.log(item.id)
-        item.id = uid2(10)
-        console.log(item.id)
-        this.resetUid(item.subset)
-      })
+          item.id = uid2(10)
+          console.log(item.id)
+          this.resetUid(item.subset)
+        })
       }
     },
     addElement(item) {
-      let addArray = new Array(this.addNumber);
-      addArray.fill(1);
+      let addArray = new Array(this.addNumber)
+      addArray.fill(1)
       item.forEach(subitem => {
-        subitem.direction = this.addDirection;
+        subitem.direction = this.addDirection
 
-        if (this.appendPosition == "subChildAppend") {
+        if (this.appendPosition == 'subChildAppend') {
           addArray.forEach(() => {
             subitem.subset.push({
               tagType: this.tagType,
               direction: this.addDirection,
               id: uid2(10),
-              className: "",
+              className: '',
               subset: []
-            });
-          });
+            })
+          })
         }
-        if (this.appendPosition == "subChildInsert") {
-          let subset = JSON.parse(JSON.stringify(subitem.subset));
+        if (this.appendPosition == 'subChildInsert') {
+          let subset = JSON.parse(JSON.stringify(subitem.subset))
 
-          subitem.subset = [];
-          
+          subitem.subset = []
+
           addArray.forEach(() => {
-            let nSubset = JSON.parse(JSON.stringify(subset));
+            let nSubset = JSON.parse(JSON.stringify(subset))
             this.resetUid(nSubset)
             subitem.subset.push({
               tagType: this.tagType,
               direction: this.addDirection,
               id: uid2(10),
-              className: "",
+              className: '',
               subset: nSubset
-            });
-          });
+            })
+          })
         }
-      });
-      this.setDatasetClassName(this.dataset, 1);
+      })
+      this.setDatasetClassName(this.dataset, 1)
     },
     jsonToHtmlStyle1(array, tagType) {
       // 操作
       // let element = null
       // if(parentElement==null){
       if (tagType == undefined) {
-        tagType = "div";
+        tagType = 'div'
       }
-      let element = document.createElement(tagType);
+      let element = document.createElement(tagType)
       // console.log('array.tagType',tagType)
       // }
       // 子级
       array.forEach(item => {
-        console.log("item.tagType", item.tagType);
-        let childElement = this.jsonToHtmlStyle1(item.subset, item.tagType);
+        console.log('item.tagType', item.tagType)
+        let childElement = this.jsonToHtmlStyle1(item.subset, item.tagType)
         childElement.className = classname(
           item.className,
-          "flex",
+          'flex',
           item.direction
-        );
-        element.appendChild(childElement);
-      });
+        )
+        element.appendChild(childElement)
+      })
       // 返回元素
-      return element;
+      return element
     },
 
     jsonToCssStyle1(array, parentHeader, parentLevel, result) {
       array.forEach((item, index) => {
-        let nodeHeader = "";
-        let sublevel = "";
-        if (parentHeader == "") {
-          nodeHeader = "." + this.containerName;
-          sublevel = "";
+        let nodeHeader = ''
+        let sublevel = ''
+        if (parentHeader == '') {
+          nodeHeader = '.' + this.containerName
+          sublevel = ''
         } else {
           nodeHeader =
-            parentHeader +
-            " " +
-            item.tagType +
-            ".s" +
-            parentLevel +
-            "-" +
-            index;
-          sublevel = parentLevel + "-" + index;
+            parentHeader + ' ' + item.tagType + '.s' + parentLevel + '-' + index
+          sublevel = parentLevel + '-' + index
         }
 
-        result.push(nodeHeader + " {}");
-        this.jsonToCssStyle1(item.subset, nodeHeader, sublevel, result);
-      });
+        result.push(nodeHeader + ' {}')
+        this.jsonToCssStyle1(item.subset, nodeHeader, sublevel, result)
+      })
     },
     setDatasetClassName(array, parentLevel) {
       array.forEach((item, index) => {
-        let sublevel = "";
+        let sublevel = ''
         if (parentLevel == 1) {
-          sublevel = "";
+          sublevel = ''
         } else {
-          sublevel = parentLevel + "-" + index;
-          item.className = "s" + sublevel;
+          sublevel = parentLevel + '-' + index
+          item.className = 's' + sublevel
         }
 
-        this.setDatasetClassName(item.subset, sublevel);
-      });
+        this.setDatasetClassName(item.subset, sublevel)
+      })
     }
   },
   computed: {
     htmlcode: function() {
       // console.log(this.jsonToHtmlStyle1(this.dataset))
-      let element = this.jsonToHtmlStyle1(this.dataset);
+      let element = this.jsonToHtmlStyle1(this.dataset)
       // // console.log(element);
       // console.log('change',this.dataset)
-      console.log("change", element.outerHTML);
-      var result = tidy_html5(element.outerHTML, options);
-      console.log("result", result);
-      return tidy_html5(element.outerHTML, options);
+      console.log('change', element.outerHTML)
+      var result = tidy_html5(element.outerHTML, options)
+      console.log('result', result)
+      return tidy_html5(element.outerHTML, options)
     },
     csscode: function() {
-      let result = [];
-      this.jsonToCssStyle1(this.dataset, "", 1, result);
+      let result = []
+      this.jsonToCssStyle1(this.dataset, '', 1, result)
       // console.log(result);
       let newResult = result.map(item => {
-        return "<p>" + item + "</p>";
-      });
-      return newResult.join("\n");
+        return '<p>' + item + '</p>'
+      })
+      return newResult.join('\n')
     }
   },
   watch: {
     containerName(newValue) {
       // console.log(v,e)
-      this.dataset[0].className = newValue;
+      this.dataset[0].className = newValue
     }
   },
   created() {
     //   let body = document.getElementsByTagName('body')
-    this.dataset[0].className = this.containerName;
-    window.addEventListener("keydown", event => {
+    this.dataset[0].className = this.containerName
+    window.addEventListener('keydown', event => {
       //   console.log(event)
       if (event.keyCode == 17) {
-        this.isMulitle = true;
+        this.isMulitle = true
       }
-    });
-    window.addEventListener("keyup", event => {
+    })
+    window.addEventListener('keyup', event => {
       //   console.log(event)
       if (event.keyCode == 17) {
-        this.isMulitle = false;
+        this.isMulitle = false
       }
-    });
+    })
   },
   components: {
     preview
   }
-};
+}
 </script>
 
 <style>
