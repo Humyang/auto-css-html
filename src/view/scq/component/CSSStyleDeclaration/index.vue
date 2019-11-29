2383b27bd6718b8058dcf67fd80e20b17a49ff59
杨雄贵 <378029633@qq.com>
2019-11-29 17:22:37 +0800

    1

---
+++ src/view/scq/component/CSSStyleDeclaration/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-tabs tab-position="left">
+    <!-- <el-tab-pane
+      v-for="(item, index) in obj"
+      :label="index"
+      :key="index"
+    ></el-tab-pane> -->
+    <!-- <el-tab-pane  >用户管理</el-tab-pane> -->
+    <!-- <el-tab-pane label="用户管理">用户管理</el-tab-pane> -->
+    <!-- <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+    <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+    <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane> -->
+  </el-tabs>
+  <!-- <div>{{ obj }}</div> -->
+</template>
+
+<script>
+export default {
+  name: "CSSStyleDeclaration",
+  //   props:[''],
+  components: {},
+  data() {
+    return {
+      obj: {}
+    };
+  },
+  methods: {},
+  computed: {},
+  watch: {
+    // example: {
+    //   handler() {},
+    //   deep: true,
+    //   immediate: true
+    // }
+  },
+  created() {},
+  mounted() {
+    for (var i in document.body.style) {
+      this.obj[i] = "";
+      // this.list.push(i);
+      // console.log(i);
+    }
+    this.obj = JSON.parse(JSON.stringify(this.obj));
+  }
+};
+</script>
+<style>
+.el-tabs--left .el-tabs__item.is-left {
+  text-align: left;
+}
+</style>
